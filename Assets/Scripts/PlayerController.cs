8a71ff9e45f07b8c372ad57bd264048e5910a611
ACER-16\apron <apron4x@gmail.com>
2024-03-18 04:21:03 +0300

    Enhanced background: Seamless looping, stops on collision, obstacles stop spawning & get destroyed off-screen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +28,14 @@ public class PlayerController : MonoBehaviour
     }
     private void OnCollisionEnter(Collision other)
     {
-        isOnGround = true;
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if(other.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over!");
+            gameOver = true;
+        }
     }
 }
